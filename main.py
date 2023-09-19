b359d3477a3f0ea5c8f418a3194dbebb7d5ea604
Bgv-Ilyas <144782975+Bgv-Ilyas@users.noreply.github.com>
2023-09-19 10:14:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+koloda = [6,7,8,9,10,2,3,4,11] * 4
+import random
+random.shuffle(koloda)
+print('Поиграем в 21?')
+count = 0
 
+while True:
+    choice = input('Будете брать карту? y/n\n')
+    if choice == 'y':
+        current = koloda.pop()
+        print('Выпало %d' %current)
+        count += current
+        if count > 21:
+            print('Вы проиграли')
+            break
+        elif count == 21:
+            print('Вы победили 21!')
+            break
+        else:
+            print('У вас %d очков.' %count)
+    elif choice == 'n':
+        print('У вас %d очков и вы закончили игру.' %count)
+        break
+
+print('До новых встречы!')
